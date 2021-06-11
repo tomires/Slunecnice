8a3e0e3e9791c8c3878b22fe5d4e605e1af46f1d
tomires <me@tomires.eu>
2021-06-11 11:23:57 +0200

    Launch Rural on startup
    Former-commit-id: 243776a322dfdef6f91ddd5f4a5323044c7c301a

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private PathCreator pathCreator;
+    [SerializeField] public EndOfPathInstruction endOfPathInstruction;
+    [SerializeField] private float distanceTravelled = 0;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private Vector3 camOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pathCreator != null)
+        {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction) + camOffset;
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+        }
+    }
+}
