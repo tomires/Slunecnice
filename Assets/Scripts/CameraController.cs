50c0409eb5395291865229b91e0126e6fb2aa9dd
tomires <me@tomires.eu>
2021-06-11 12:01:07 +0200

    Add option to speed up cam movement
    Former-commit-id: b9f2f58c51a47d419978ba013d290b39490074c5

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -20,5 +20,12 @@ public class CameraController : MonoBehaviour
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction) + camOffset;
             transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
         }
+
+        if(OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
+            speed = 10.0f;
+        else if (OVRInput.Get(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+            speed = 0.0f;
+        else
+            speed = 1.0f;
     }
 }
